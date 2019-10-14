967ed40ad2cf3ea8b7def18f1b8ff0fc8ad262bf
zongkaili <zongkaili@idealsee.cn>
2019-10-14 22:53:18 +0800

    增加svg示例

---
+++ app/src/main/java/com/kelly/test/widget/svg/SvgChinaMapActivity.kt
@@ -0,0 +1,13 @@
+package com.kelly.test.widget.svg
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kelly.test.R
+
+class SvgChinaMapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_svg_chinamap)
+    }
+}
