d249cedbf0c754816648a901735c5e533af06042
zongkaili <zongkaili@idealsee.cn>
2019-10-27 11:05:35 +0800

    补充commit

--- app/src/main/java/com/kelly/test/widget/colorfilter/ColorFilterTestActivity.kt
+++ app/src/main/java/com/kelly/test/widget/colorfilter/ColorFilterTestActivity.kt
@@ -3,12 +3,11 @@ package com.kelly.test.widget.colorfilter
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
-import com.kelly.test.widget.test.DragView
 
 class ColorFilterTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(DragView(this))
+        setContentView(ColorFilterView(this))
     }
 }
