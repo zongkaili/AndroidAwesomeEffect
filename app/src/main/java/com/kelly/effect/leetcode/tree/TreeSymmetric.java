25472886488539e1dc374828af128b0495eeddfc
zongkaili <zongkaili@zuoyebang.com>
2021-11-21 16:22:43 +0800

    算法：对称二叉树 & 最小栈 & 只出现一次的数字

---
+++ app/src/main/java/com/kelly/effect/leetcode/tree/TreeSymmetric.java
@@ -0,0 +1,82 @@
+package com.kelly.effect.leetcode.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 101. 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * <p>
+ * 但是下面这个[1,2,2,null,3,null,3] 则不是镜像对称的:
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ */
+public class TreeSymmetric {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() { }
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    /**
+     * 方法一. 递归
+     */
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+             return false;
+        }
+
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    /**
+     * 方法二：迭代
+     */
+    private boolean check2(TreeNode u, TreeNode v) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(u);
+        q.offer(v);
+        while (!q.isEmpty()) {
+            u = q.poll();
+            v = q.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if ((u == null || v == null) || u.val != v.val) {
+                return false;
+            }
+            q.offer(u.left);
+            q.offer(v.right);
+
+            q.offer(u.right);
+            q.offer(v.left);
+        }
+        return true;
+    }
+
+}
