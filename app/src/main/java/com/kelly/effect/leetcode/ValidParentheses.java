813eed37f36a367e1e4bf0c5727f810e3f2b7c0d
zongkaili <zongkaili@idealsee.cn>
2022-03-01 08:01:26 +0800

    算法：两个队列实现一个栈

--- app/src/main/java/com/kelly/effect/leetcode/ValidParentheses.java
+++ app/src/main/java/com/kelly/effect/leetcode/ValidParentheses.java
@@ -8,36 +8,29 @@ import java.util.Stack;
  * data: 2020/3/15
  * desc: 20.有效的括号
  * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- *
+ * <p>
  * 有效字符串需满足：
- *
- *     左括号必须用相同类型的右括号闭合。
- *     左括号必须以正确的顺序闭合。
- *
+ * 1.左括号必须用相同类型的右括号闭合。
+ * 2.左括号必须以正确的顺序闭合。
  * 注意空字符串可被认为是有效字符串。
- *
+ * <p>
  * 示例 1:
- *
  * 输入: "()"
  * 输出: true
- *
+ * <p>
  * 示例 2:
- *
  * 输入: "()[]{}"
  * 输出: true
- *
+ * <p>
  * 示例 3:
- *
  * 输入: "(]"
  * 输出: false
- *
+ * <p>
  * 示例 4:
- *
  * 输入: "([)]"
  * 输出: false
- *
+ * <p>
  * 示例 5:
- *
  * 输入: "{[]}"
  * 输出: true
  */
@@ -46,24 +39,32 @@ public class ValidParentheses {
         String intput = "{[{}[]]";
         System.out.println("intput = " + intput + ", isValid = " + isValid(intput));
     }
-    private static boolean isValid(String s) {
-        //solution1 : 数组
-//        char[] stack = new char[s.length() + 1];
-//        int top = 1;
-//        for (char c : s.toCharArray()) {
-//            if (c == '(' || c == '[' || c == '{') {
-//                stack[top++] = c;
-//            } else if (c == ')' && stack[--top] != '(') {
-//                return false;
-//            } else if (c == ']' && stack[--top] != '[') {
-//                return false;
-//            } else if (c == '}' && stack[--top] != '{') {
-//                return false;
-//            }
-//        }
-//        return top == 1;
 
-        //solution2 : 栈Stack
+    /**
+     * 方法1：数组
+     */
+    private static boolean isValid(String s) {
+        char[] stack = new char[s.length() + 1];
+        int top = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack[top++] = c;
+            } else if (c == ')' && stack[--top] != '(') {
+                return false;
+            } else if (c == ']' && stack[--top] != '[') {
+                return false;
+            } else if (c == '}' && stack[--top] != '{') {
+                return false;
+            }
+        }
+        return top == 1;
+    }
+
+    /**
+     * 方法2：栈Stack
+     * s = "{[{}[]]"
+     */
+    private static boolean isValid2(String s) {
         HashMap<Character, Character> mappings = new HashMap<>();
         mappings.put(')', '(');
         mappings.put(']', '[');
