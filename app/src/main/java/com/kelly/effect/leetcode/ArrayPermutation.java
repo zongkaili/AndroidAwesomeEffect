a85411234fcb60ff8e945fb5982b68e58753f057
zongkaili <zongkaili@idealsee.cn>
2022-03-06 21:20:06 +0800

    算法：数组全排列

--- app/src/main/java/com/kelly/effect/leetcode/ArrayPermutation.java
+++ app/src/main/java/com/kelly/effect/leetcode/ArrayPermutation.java
@@ -2,14 +2,26 @@ package com.kelly.effect.leetcode;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 数组全排列
+ * 46.全排列
+ * 给定一个不含重复数字的数组 nums，返回其所有可能的全排列。你可以按任意顺序返回答案。
+ * 示例 1：
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
  *
- * @author zyb
+ * 示例 2：
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ *
+ * 示例 3：
+ * 输入：nums = [1]
+ * 输出：[[1]]
  */
 public class ArrayPermutation {
 
@@ -75,8 +87,6 @@ public class ArrayPermutation {
      * 路径:记录在 track 中
      * 选择列表:nums 中不存在于 track 的那些元素
      * 结束条件:nums 中的元素全都在 track 中出现
-     * @param nums
-     * @param track
      */
     private static void backtrack(int[] nums, LinkedList<Integer> track) {
         // 触发结束条件
@@ -94,6 +104,44 @@ public class ArrayPermutation {
             backtrack(nums, track);
             //取消选择
             track.removeLast();
+        }
+    }
+
+    /**
+     * 官解：回溯--------------------------------
+     */
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        List<Integer> output = new ArrayList<>();
+        for (int num : nums) {
+            output.add(num);
+        }
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    /**
+     * 回溯法：一种通过探索所有可能的候选解来找出所有的解的算法。如果候选解被确认不是一个解（或者至少不是最后一个解），
+     *       回溯算法会通过在上一步进行一些变化抛弃该解，即回溯并且再次尝试。
+     * 时间复杂度：O(n×n!)，其中 n 为序列的长度。
+     * 空间复杂度：O(n)，其中 n 为序列的长度。除答案数组以外，递归函数在递归过程中需要为每一层递归函数分配栈空间，
+     *           所以这里需要额外的空间且该空间取决于递归的深度，这里可知递归调用深度为 O(n)。
+     */
+    public void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
         }
     }
 
