2d6f5f372d7661d9929c071f448dbd1134b8bd54
zongkaili <zongkaili@zuoyebang.com>
2022-04-02 15:32:02 +0800

    设计模式：Builder & Decorator

---
+++ app/src/main/java/com/kelly/effect/design_mode/BuilderPattern.java
@@ -0,0 +1,178 @@
+package com.kelly.effect.design_mode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Copyright (c) 2014-2022 Zuoyebang, All rights reserved.
+ *
+ * @author zongkaili | zongkaili@zuoyebang.com
+ * @version 1.0.0 | 2022/4/2 | zongkaili 初始版本
+ * @description
+ * @date 2022/4/2 3:19 下午
+ */
+public class BuilderPattern {
+    public interface Item {
+        String name();
+        Packing packing();
+        float price();
+    }
+
+    public interface Packing {
+        String pack();
+    }
+
+    public static class Wrapper implements Packing {
+        @Override
+        public String pack() {
+            return "Wrapper";
+        }
+    }
+
+    public static class Bottle implements Packing {
+        @Override
+        public String pack() {
+            return "Bottle";
+        }
+    }
+
+    public static abstract class Burger implements Item {
+        @Override
+        public Packing packing() {
+            // 默认打包方式：袋装
+            return new Wrapper();
+        }
+
+        @Override
+        public abstract float price();
+    }
+
+    public static abstract class ColdDrink implements Item {
+        @Override
+        public Packing packing() {
+            // 默认打包方式：瓶装
+            return new Bottle();
+        }
+
+        @Override
+        public abstract float price();
+    }
+
+    public static class VegBurger extends Burger {
+        @Override
+        public float price() {
+            return 25.0f;
+        }
+
+        @Override
+        public String name() {
+            return "Veg Burger";
+        }
+    }
+
+    public static class ChickenBurger extends Burger {
+        @Override
+        public float price() {
+            return 50.5f;
+        }
+
+        @Override
+        public String name() {
+            return "Chicken Burger";
+        }
+    }
+
+    public static class Coke extends ColdDrink {
+        @Override
+        public float price() {
+            return 30.0f;
+        }
+
+        @Override
+        public String name() {
+            return "Coke";
+        }
+    }
+
+    public static class Pepsi extends ColdDrink {
+        @Override
+        public float price() {
+            return 35.0f;
+        }
+
+        @Override
+        public String name() {
+            return "Pepsi";
+        }
+    }
+
+    public static class Meal {
+        private final List<Item> items = new ArrayList<>();
+
+        public void addItem(Item item){
+            items.add(item);
+        }
+
+        public float getCost(){
+            float cost = 0.0f;
+            for (Item item : items) {
+                cost += item.price();
+            }
+            return cost;
+        }
+
+        public void showItems(){
+            for (Item item : items) {
+                System.out.print("Item : "+item.name());
+                System.out.print(", Packing : "+item.packing().pack());
+                System.out.println(", Price : "+item.price());
+            }
+        }
+    }
+
+    /**
+     * 实际的 Builder 类，负责创建 Meal
+     */
+    public static class MealBuilder {
+        public Meal prepareVegMeal (){
+            Meal meal = new Meal();
+            meal.addItem(new VegBurger());
+            meal.addItem(new Coke());
+            return meal;
+        }
+
+        public Meal prepareNonVegMeal (){
+            Meal meal = new Meal();
+            meal.addItem(new ChickenBurger());
+            meal.addItem(new Pepsi());
+            return meal;
+        }
+    }
+
+    public static void main(String[] args) {
+        MealBuilder mealBuilder = new MealBuilder();
+
+        Meal vegMeal = mealBuilder.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost: " + vegMeal.getCost());
+
+        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+        System.out.println("\n\nNon-Veg Meal");
+        nonVegMeal.showItems();
+        System.out.println("Total Cost: " + nonVegMeal.getCost());
+    }
+    // 输出如下：
+    /*
+    Veg Meal
+    Item : Veg Burger, Packing : Wrapper, Price : 25.0
+    Item : Coke, Packing : Bottle, Price : 30.0
+    Total Cost: 55.0
+
+    Non-Veg Meal
+    Item : Chicken Burger, Packing : Wrapper, Price : 50.5
+    Item : Pepsi, Packing : Bottle, Price : 35.0
+    Total Cost: 85.5
+     */
+
+}
---
+++ app/src/main/java/com/kelly/effect/design_mode/DecoratorPattern.java
@@ -0,0 +1,80 @@
+package com.kelly.effect.design_mode;
+
+/**
+ * 装饰器模式（Decorator Pattern）允许向一个现有的对象添加新的功能，同时又不改变其结构。
+ * 这种类型的设计模式属于结构型模式，它是作为现有的类的一个包装。
+ * 这种模式创建了一个装饰类，用来包装原有的类，并在保持类方法签名完整性的前提下，提供了额外的功能。
+ * 我们通过下面的实例来演示装饰器模式的用法。其中，我们将把一个形状装饰上不同的颜色，同时又不改变形状类。
+ * @author zyb
+ */
+public class DecoratorPattern {
+    public interface Shape {
+        void draw();
+    }
+
+    public static class Rectangle implements Shape {
+        @Override
+        public void draw() {
+            System.out.println("Shape: Rectangle");
+        }
+    }
+
+    public static class Circle implements Shape {
+        @Override
+        public void draw() {
+            System.out.println("Shape: Circle");
+        }
+    }
+
+    /**
+     * 实现了 Shape 接口的抽象装饰类
+     */
+    public static abstract class ShapeDecorator implements Shape {
+        protected Shape decoratedShape;
+
+        public ShapeDecorator(Shape decoratedShape){
+            this.decoratedShape = decoratedShape;
+        }
+
+        @Override
+        public void draw(){
+            decoratedShape.draw();
+        }
+    }
+
+    /**
+     * 扩展了 ShapeDecorator 类的实体装饰类
+     */
+    public static class RedShapeDecorator extends ShapeDecorator {
+
+        public RedShapeDecorator(Shape decoratedShape) {
+            super(decoratedShape);
+        }
+
+        @Override
+        public void draw() {
+            decoratedShape.draw();
+            setRedBorder(decoratedShape);
+        }
+
+        private void setRedBorder(Shape decoratedShape){
+            System.out.println("Border Color: Red");
+        }
+    }
+
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        ShapeDecorator redCircle = new RedShapeDecorator(new Circle());
+        ShapeDecorator redRectangle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle with normal border");
+        circle.draw(); // Shape: Circle
+
+        System.out.println("\nCircle of red border");
+        redCircle.draw(); // Shape: Circle
+                          // Border Color: Red
+        System.out.println("\nRectangle of red border");
+        redRectangle.draw(); // Shape: Rectangle
+                             // Border Color: Red
+    }
+}
